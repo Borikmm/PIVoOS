012d0333f6adc52640e8343c60f182697659b514
borikmm <valera.nalabordin000@mail.ru>
2023-05-04 20:04:30 +0300

    version 0.1

--- Start.py
+++ Start.py
@@ -4,60 +4,101 @@ import time
 import random
 from pygame.color import THECOLORS
 
-
+# for debigung
 pygame.init()
-n = "?"
-a = 0
-b = 0
-width = 500
-height = 500
-cube_size = 20
-skok_cube = ((width*height)//cube_size**2)
-ckok_uge = 0
-po_y = height/cube_size
-po_x = width/cube_size
 
-screen = pygame.display.set_mode((width, height))
 
-# Text Antialias = сглаживание
-screen.fill(THECOLORS['white'])
-font = pygame.font.SysFont('couriernew', cube_size-10)
-#font = pygame.font.SysFont('couriernew', cube_size)
-#text = font.render(str(f'{n}'), True, THECOLORS['green'])
-#screen.blit(text, (50, 50))
 
-def apr(cube_size):
-    global a, b, width, height, skok_cube, ckok_uge, po_y, po_x, n, font
-    while ckok_uge <= skok_cube:
-        r = pygame.Rect(a, b, cube_size, cube_size)
-        pygame.draw.rect(screen, (255, 0, 0), r, 1)
-        text = font.render(str(f'{n}'), True, THECOLORS['black'])
-        screen.blit(text, (a, b))
 
-        if po_x == 0:
-            if po_y == 0:
-                pass
-            else:
-                a = 0
-                po_x = width/cube_size
-                po_y -= 1
-                b += cube_size
-        else:
-            po_y = height/cube_size
-            po_x -= 1
-            a += cube_size
-            ckok_uge += 1
-  #          n += 1
-        pygame.display.flip()
-        print("otr")
 
-apr(cube_size)
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-        if event.type == pygame.KEYDOWN:
-            color = (255, 255, 255)
-    pygame.display.flip()
+
+
+
+class Desktop:
+    color = {
+        "black": [0, 0, 0],
+        "white": [255, 255, 255],
+        "red": [255, 0, 0],
+        "green": [0, 255, 0],
+        "blue": [0, 0, 255]
+    }
+
+    pi = 3.141592653
+
+    def __init__(self):
+
+        # for debuging
+        self.screen = pygame.display.set_mode((800, 800))
+        self.screen.fill(THECOLORS['black'])
+
+        clock = pygame.time.Clock()
+        draw = False
+
+        self.font = pygame.font.Font(None, 25)
+
+        self.objects = {
+            "windows": {},
+            "labels": {
+                "pusk": (lambda: pygame.draw.circle(self.screen, self.color["blue"], (20, 790), 20), lambda: None),
+            }
+        }
+
+        while True:
+            clock.tick(30)
+
+
+            for event in pygame.event.get():
+
+                if draw:
+                    pygame.draw.circle(self.screen, self.color["blue"], event.pos, 20)
+                    pygame.draw.rect(self.screen, self.color["red"], [20, 20, event.pos[0], event.pos[1]], 2)
+
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    print("hello world")
+
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if event.button == 1:
+                        draw = True
+
+                if event.type == pygame.MOUSEBUTTONUP:
+                    if event.button == 1:
+                        draw = False
+
+            #screen.fill(self.color["black"])
+
+            self.window_otr()
+
+            # pygame.draw.line(self.screen, self.color["white"], [0, 0], [100, 100], 5)
+
+            # y_offset = 0
+            # while y_offset < 100:
+            #     pygame.draw.line(self.screen, self.color["red"], [0, 10 + y_offset], [100, 110 + y_offset], 5)
+            #     y_offset += 10
+            #
+            # text = self.font.render("My text", True, self.color["white"])
+            #
+            # # Рисуем изображение текста на экран в точке (250, 250)
+            # self.screen.blit(text, [250, 250])
+            #
+            # # Рисуем прямоугольник
+            # pygame.draw.rect(self.screen, self.color["red"], [20, 20, 250, 100], 2)
+            #
+            pygame.display.flip()
+
+    def window_otr(self):
+        for a in self.objects["windows"]:
+            a()
+        for name_label, atr_label in self.objects["labels"].items():
+            otr_label = atr_label[0]
+            com_label = atr_label[1]
+
+            otr_label()
+
+
+
+if __name__ == "__main__":
+    Desktop()
