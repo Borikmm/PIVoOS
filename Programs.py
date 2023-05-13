85213aad9bc8b1a4d6a1e732441e1ead23b08856
borikmm <valera.nalabordin000@mail.ru>
2023-05-13 14:02:22 +0300

    version 0.3

--- Programs.py
+++ Programs.py
@@ -11,13 +11,12 @@ class programs:
         return value
 
     @staticmethod
-    def add_panel_pusk(screen, colors):
-        pos_object = None
-        value1 = (
-            [lambda: pygame.draw.rect(screen, colors["green"], pos_object, radius), lambda: func(screen, color, pos_object, radius)],
-            lambda: "panel",
-            (pos_object[0], pos_object[1], radius, color) if str(func) == "<built-in function circle>" else (pos_object[0], pos_object[1], pos_object[2], pos_object[3], color)
-        )
+    def add_panel_pusk(screen, colors, func):
+        value = [
+            ["rect1", lambda: func(pygame.draw.rect, screen, colors["white"], (200, 200, 20, 20), 2)],
+            ["rect2", lambda: func(pygame.draw.rect, screen, colors["white"], (200, 200, 10, 10), 2)],
+            ["rect3", lambda: func(pygame.draw.rect, screen, colors["white"], (200, 200, 5, 5), 2)]
+        ]
         return value
 
 
--- Start.py
+++ Start.py
@@ -9,7 +9,55 @@ import Programs
 pygame.init()
 
 
-class Desktop(Programs.programs):
+class Tools:
+    def check(self, type_label, names_label, pos_cursor, pos, com_label):
+        match type_label:
+            case "circle":
+                rad = pos[2]
+                color = pos[3]
+                if pos[0] - rad < pos_cursor[0] < (pos[0] + rad) and pos[1] - rad < pos_cursor[1] < pos[1] + rad:
+                    match com_label():
+                        case "move":
+                            if self.draw:
+                                if self.test:
+                                    self.pos_need = (pos[0], pos[1])
+                                    self.test = False
+                                self.objects["windows"][names_label] = self.add_window(pygame.draw.circle, self.screen,
+                                                                                       color,
+                                                                                       (pos_cursor[0] + (
+                                                                                                   self.pos_need[0] -
+                                                                                                   self.pos_cursor[0]),
+                                                                                        pos_cursor[1] + (
+                                                                                                    self.pos_need[1] -
+                                                                                                    self.pos_cursor[
+                                                                                                        1])),
+                                                                                       rad)
+            case "rect":
+                color = pos[4]
+                width = pos[2]
+                height = pos[3]
+                if pos[0] < pos_cursor[0] < (pos[0] + width) and pos[1] < pos_cursor[1] < pos[1] + height:
+                    match com_label():
+                        case "move":
+                            if self.draw:
+                                if self.test:
+                                    self.pos_need = (pos[0], pos[1])
+                                    self.test = False
+                                self.objects["windows"][names_label] = self.add_window(pygame.draw.rect, self.screen,
+                                                                                       color,
+                                                                                       (pos_cursor[0] + (
+                                                                                                   self.pos_need[0] -
+                                                                                                   self.pos_cursor[0]),
+                                                                                        pos_cursor[1] + (
+                                                                                                    self.pos_need[1] -
+                                                                                                    self.pos_cursor[1]),
+                                                                                        width, height),
+                                                                                       self.FIXED_FRAME)
+
+
+
+
+class Desktop(Programs.programs, Tools):
     color = {
         "black": [0, 0, 0],
         "white": [255, 255, 255],
@@ -24,9 +72,8 @@ class Desktop(Programs.programs):
 
     def __init__(self):
 
-
-
-        super().__init__()
+        Programs.programs.__init__(self)
+        Tools.__init__(self)
 
         # for debuging
         self.screen = pygame.display.set_mode((800, 800))
@@ -42,16 +89,16 @@ class Desktop(Programs.programs):
 
             },
             "labels": {
-                "panel-rect": (lambda: pygame.draw.rect(self.screen, self.color["blue"], (20, 750, 30, 40), 2),
+                "panel-rect-solo": (lambda: pygame.draw.rect(self.screen, self.color["blue"], (20, 750, 30, 40), 2),
                          lambda: self.add_window(pygame.draw.rect, self.screen, self.color["red"], (50, 400, 40, 40), 2),
                          (20, 750, 30, 40, self.color["red"])),
-                "panel1-rect": (lambda: pygame.draw.rect(self.screen, self.color["green"], (20, 700, 30, 40), 2),
-                               lambda: self.add_panel_pusk(self.screen, self.color, (100, 600, 40, 40), 2),
+                "panel1-rect-many": (lambda: pygame.draw.rect(self.screen, self.color["green"], (20, 700, 30, 40), 2),
+                               lambda: self.add_panel_pusk(self.screen, self.color, self.add_window),
                                (20, 700, 30, 40, self.color["green"])),
-                "pusk-circle": (lambda: pygame.draw.circle(self.screen, self.color["blue"], (80, 760), 20),
+                "pusk-circle-solo": (lambda: pygame.draw.circle(self.screen, self.color["blue"], (80, 760), 20),
                          lambda: self.add_window(pygame.draw.circle, self.screen, self.color["red"], (200, 400), 20),
                          (80, 760, 20, self.color["red"])),
-                "pusk1-circle": (lambda: pygame.draw.circle(self.screen, self.color["blue"], (200, 760), 20),
+                "pusk1-circle-solo": (lambda: pygame.draw.circle(self.screen, self.color["blue"], (200, 760), 20),
                          lambda: self.add_window(pygame.draw.circle, self.screen, self.color["blue"], (300, 400), 20),
                          (200, 760, 20, self.color["blue"])),
             }
@@ -97,12 +144,17 @@ class Desktop(Programs.programs):
 
         for name_label, atr_label in self.objects["windows"].items():
             otr_label = atr_label[0]
-            if len(otr_label()) > 4:
-                for a in otr_label:
-                    a()
-            else:
-                otr_label()
 
+            if name_label.split("-")[2] == "solo":
+                if len(otr_label()) > 4:
+                    for a in otr_label:
+                        a()
+                else:
+                    otr_label()
+            elif name_label.split("-")[2] == "many":
+
+                for i in atr_label:
+                    i[1]()[0]()
 
         for name_label, atr_label in self.objects["labels"].items():
             otr_label = atr_label[0]
@@ -112,42 +164,26 @@ class Desktop(Programs.programs):
     def check_tap(self, pos_cursor):
         for names_label, atr_label in self.objects["windows"].items():
 
-            type_label, name_label = names_label.split("-")[1], names_label.split("-")[0]
+            type_label, name_label, col_element = names_label.split("-")[1], names_label.split("-")[0], names_label.split("-")[2]
 
             pos = atr_label[2]
             com_label = atr_label[1]
 
-            match type_label:
-                case "circle":
-                    rad = pos[2]
-                    color = pos[3]
-                    if pos[0] - rad < pos_cursor[0] < (pos[0] + rad) and pos[1] - rad < pos_cursor[1] < pos[1] + rad:
-                        if com_label() == "move":
-                            if self.draw:
-                                if self.test:
-                                    self.pos_need = (pos[0], pos[1])
-                                    self.test = False
-                                self.objects["windows"][names_label] = self.add_window(pygame.draw.circle, self.screen,
-                                                                                      color,
-                                                                                      (pos_cursor[0] + (self.pos_need[0] - self.pos_cursor[0]) , pos_cursor[1] + (self.pos_need[1] - self.pos_cursor[1])),
-                                                                                      rad)
-                case "rect":
-                    color = pos[4]
-                    width = pos[2]
-                    height = pos[3]
-                    if pos[0] < pos_cursor[0] < (pos[0] + width) and pos[1] < pos_cursor[1] < pos[1] + height:
-                        match com_label():
-                            case "move":
-                                if self.draw:
-                                    if self.test:
-                                        self.pos_need = (pos[0], pos[1])
-                                        self.test = False
-                                    self.objects["windows"][names_label] = self.add_window(pygame.draw.rect, self.screen,
-                                                                                           color,
-                                                                                           (pos_cursor[0] + (self.pos_need[0] - self.pos_cursor[0]), pos_cursor[1] + (self.pos_need[1] - self.pos_cursor[1]), width, height),
-                                                                                           self.FIXED_FRAME)
-                            case "panel":
-                                print("hello_world")
+            match col_element:
+                case "solo":
+                    self.check(type_label, names_label, pos_cursor, pos, com_label)
+                case "many":
+                    for atr_label2 in atr_label:
+
+                        try:
+                            type_label = atr_label2[0]
+                            pos = atr_label2[1][2]
+                            com_label = atr_label2[1][1]
+                        except:
+                            pass
+
+                        self.check(type_label, names_label, pos_cursor, pos, com_label)
+
 
         for names_label, atr_label in self.objects["labels"].items():
 
@@ -166,6 +202,7 @@ class Desktop(Programs.programs):
                     height = pos[3]
                     if pos[0] < pos_cursor[0] < (pos[0] + width) and pos[1] < pos_cursor[1] < pos[1] + height:
                         self.objects["windows"][names_label] = com_label()
+
 
 if __name__ == "__main__":
     Desktop()
